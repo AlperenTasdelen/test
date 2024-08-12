e06df1e43e7085f1def6f3844a2f4ee8cf73865d
AlperenTasdelen <alperen.tasdelen@metu.edu.tr>
2024-08-12 16:52:02 +0300

    field management needed

--- src/main/java/solrtest/model/DocumentModel.java
+++ src/main/java/solrtest/model/DocumentModel.java
@@ -3,17 +3,32 @@ package solrtest.model;
 import lombok.*;
 import java.util.Date;
 
+import org.apache.solr.client.solrj.beans.Field;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class LogModel {
+    @Field
     private int id;
+
+    @Field
     private String logLevel;
+    
+    @Field
     private String logType;
+
+    @Field
     private String hardwareName;
+    
+    @Field
     private String functionType;
+    
+    @Field
     private Date logDate;
+    
+    @Field
     private String context;
 }
